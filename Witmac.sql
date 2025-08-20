c2f6c9118c6ccb8a79f5cb63f024484aad03d1a5
Gandrall <li_alfonsovaldez@unca.edu.mx>
2025-08-20 11:47:42 -0600

    Update Witmac.sql

--- Witmac.sql
+++ Witmac.sql
@@ -5,6 +5,24 @@ CREATE TABLE empresa (
     direccion TEXT NOT NULL
 );
 
+-- TABLA ADMINISTRADOR
+CREATE TABLE administrador (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    empresa_id INT REFERENCES empresa(id)
+);
+
+-- TABLA EMPLEADO
+CREATE TABLE empleado (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    empresa_id INT REFERENCES empresa(id)
+);
+
 -- TABLA CENTRAL
 CREATE TABLE central (
     id SERIAL PRIMARY KEY,
